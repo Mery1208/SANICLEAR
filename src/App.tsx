3e9ad4d741af88d5d3c928aca934b1d0057241cc
Mery <mceballosm04@iesalbarregas.es>
2026-05-25 12:49:22 +0200

    vercel para analiticas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { AuthProvider } from './context/AuthContext';
 import AppRouter from './routes/AppRouter';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App(): React.JSX.Element {
   return (
     <AuthProvider>
       <AppRouter />
+      <Analytics />
+      <SpeedInsights />
     </AuthProvider>
   );
 }
